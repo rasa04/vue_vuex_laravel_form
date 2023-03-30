e87567ef68316b924359a5b194b2b9429637ac13
Rasa <abdurasul2004.02@gmail.com>
2023-03-30 15:31:29 +0500

    saving with factory

--- server/app/Http/Controllers/Form/StoreController.php
+++ server/app/Http/Controllers/Form/StoreController.php
@@ -5,15 +5,15 @@ namespace App\Http\Controllers\Form;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Form\StoreRequest;
 use App\Http\Resources\Form\FormResource;
-use Illuminate\Http\Request;
 use App\Models\Form;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
-        $form = $request->validated();
-        $new_form = Form::create($form);
-        return FormResource::make($new_form)->resolve();
+        $data = $request->validated();
+        $form = Form::factory()->make($data);
+        $form->save();
+        return FormResource::make($form)->resolve();
     }
 }
--- server/app/Models/Form.php
+++ server/app/Models/Form.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Form extends Model
 {
     use HasFactory;
+    
     protected $fillable = [
         'name',
         'form'
